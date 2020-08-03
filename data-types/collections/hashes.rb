ee65463c0434d2de335e94b8782bbe1a2e54a125
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-08-03 11:23:04 -0600

    Complete hashes exercises

--- data-types/collections/hashes.rb
+++ data-types/collections/hashes.rb
@@ -7,13 +7,13 @@ class HashesTest < Minitest::Test
 
   def test_1
     # In the line below, create a new empty hash called empty
-    empty = ______
+    empty = Hash.new
     assert_equal ({}), empty
   end
 
   def test_2
     # In the line below, create an empty hash with a default value 0
-    empty = ______
+    empty = Hash.new(0)
     assert_equal 0, empty[:not_found]
   end
 
@@ -21,7 +21,7 @@ class HashesTest < Minitest::Test
     # In the line below, create a hash called ages. The hash should
     # have a key of "ben" with a value of 4 and a key of "kelly" with
     # a value of 6
-    ages = ______
+    ages = {"ben" => 4, "kelly" => 6}
     assert_equal 2, ages.length
     assert_equal 4, ages["ben"]
     assert_equal 6, ages["kelly"]
@@ -31,14 +31,14 @@ class HashesTest < Minitest::Test
     # In the line below, create a hash called ages. The hash should
     # have a key of :ben with a value of 4 and a key of :kelly with
     # a value of 6.
-    ages = ______
+    ages = {:ben => 4, :kelly => 6}
     assert_equal 2, ages.length
     assert_equal 4, ages[:ben]
     assert_equal 6, ages[:kelly]
 
     # There are two different syntaxes for doing this. Use the other way to
     # achieve the same result
-    ages = ______
+    ages = {ben: 4, kelly: 6}
     assert_equal 2, ages.length
     assert_equal 4, ages[:ben]
     assert_equal 6, ages[:kelly]
@@ -48,7 +48,10 @@ class HashesTest < Minitest::Test
     # In the line below, create a new hash with
     # default values of zero
     # create a "tomatoes" key and a :carrots key
-    ingredients = ______
+    ingredients = Hash.new(0)
+    ingredients["tomatoes"] = 0
+    ingredients[:carrots] = 0
+
 
     assert_equal 0, ingredients["tomatoes"]
     assert_equal 0, ingredients[:carrots]
@@ -61,7 +64,7 @@ class HashesTest < Minitest::Test
     }
     # Using the books hash defined above,
     # access the value "Grapes of Wrath"  in the line below
-    grapes = ______
+    grapes = books["John Steinbeck"]
     assert_equal "Grapes of Wrath", grapes
   end
 
@@ -74,7 +77,7 @@ class HashesTest < Minitest::Test
     # add a new key of "Ernest Hemmingway"
     # with a value of "For Whom the Bell Tolls"
     # in the line below
-    ______
+    books["Ernest Hemmingway"] = "For Whom the Bell Tolls"
     expected = {
       "John Steinbeck" => "Grapes of Wrath",
       "Harper Lee" => "To Kill a Mockingbird",
@@ -91,7 +94,7 @@ class HashesTest < Minitest::Test
     # Using the books hash defined above,
     # change the value associated with
     # "John Steinbeck" to "Of Mice and Men"
-    ______
+    books["John Steinbeck"] = "Of Mice and Men"
     expected = {
       "John Steinbeck" => "Of Mice and Men",
       "Harper Lee" => "To Kill a Mockingbird"
@@ -106,7 +109,7 @@ class HashesTest < Minitest::Test
     }
     # Using the books hash defined above,
     # delete the key "Harper Lee"
-    ______
+    books.delete("Harper Lee")
     expected = {
       "John Steinbeck" => "Grapes of Wrath"
     }
@@ -120,7 +123,7 @@ class HashesTest < Minitest::Test
     }
     # Using the books hash defined above,
     # access the value "Grapes of Wrath"  in the line below
-    grapes = ______
+    grapes = books[:John_Steinbeck]
     assert_equal "Grapes of Wrath", grapes
   end
 
@@ -133,7 +136,7 @@ class HashesTest < Minitest::Test
     # add a new key of :Ernest_Hemmingway
     # with a value of "For Whom the Bell Tolls"
     # in the line below
-    ______
+    books[:Ernest_Hemmingway] = "For Whom the Bell Tolls"
     expected = {
       John_Steinbeck: "Grapes of Wrath",
       Harper_Lee: "To Kill a Mockingbird",
@@ -150,7 +153,7 @@ class HashesTest < Minitest::Test
     # Using the books hash defined above,
     # change the value associated with
     # :John_Steinbeck to "Of Mice and Men"
-    ______
+    books[:John_Steinbeck] = "Of Mice and Men"
     expected =  {
       John_Steinbeck: "Of Mice and Men",
       Harper_Lee: "To Kill a Mockingbird"
@@ -165,7 +168,7 @@ class HashesTest < Minitest::Test
     }
     # Using the books hash defined above,
     # delete the key :Harper_Lee
-    ______
+    books.delete(:Harper_Lee)
     expected = {
       John_Steinbeck: "Grapes of Wrath"
     }
@@ -180,7 +183,7 @@ class HashesTest < Minitest::Test
     }
     # Using the ages hash defined above
     # increment Julio's age by one
-    ______
+    ages["Julio"] = 9
     assert_equal 9, ages["Julio"]
   end
 
@@ -192,7 +195,7 @@ class HashesTest < Minitest::Test
     }
     # Using the ages hash defined above
     # increment Julio's age by one
-    ______
+    ages[:Julio] = 9
     assert_equal 9, ages[:Julio]
   end
 
@@ -204,7 +207,7 @@ class HashesTest < Minitest::Test
     }
     # Using the ages hash defined above
     # get an array of all the names
-    names = ______
+    names = ages.keys
     assert_equal [:Jimmy, :Julio, :Juliet], names
   end
 
@@ -216,7 +219,7 @@ class HashesTest < Minitest::Test
     }
     # Using the ages hash defined above
     # get an array of all the ages
-    age_list = ______
+    age_list = ages.values
     assert_equal [4, 8, 9], age_list
   end
 
@@ -228,7 +231,7 @@ class HashesTest < Minitest::Test
     }
     # Using the ages hash defined above
     # find the number of key/value pairs
-    num_pairs = ______
+    num_pairs = ages.length
     assert_equal 3, num_pairs
   end
 
@@ -240,12 +243,12 @@ class HashesTest < Minitest::Test
     }
     # Call a method on the ages hash defined above
     # to figure out if :Jimmy is a key
-    jimmy_in_hash = ______
+    jimmy_in_hash = ages.include?(:Jimmy)
     assert_equal true, jimmy_in_hash
 
     # Now figure out if :Jackie is in the hash
 
-    jackie_in_hash = ______
+    jackie_in_hash = ages.has_key?(:Jackie)
     assert_equal false, jackie_in_hash
   end
 
@@ -257,7 +260,7 @@ class HashesTest < Minitest::Test
     }
     # Call a method on the ages hash defined above
     # to make the keys the values and vice versa
-    opposite = _____
+    opposite = ages.invert
     expected = {
       4 => :Jimmy,
       8 => :Julio,
